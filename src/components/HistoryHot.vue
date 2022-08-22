9dac4386a234d813a1ab8a9eb399769a53d64bf1
tangpingxtc <2984286838@qq.com>
2022-08-22 22:27:02 +0800

    index on master: 2ba88ff Merge pull request #12 from Ryougi-Shio/master

---
+++ src/components/HistoryHot.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="his-hot">
+        <div class="hd">
+            <h4>历史记录</h4>
+            <van-icon name="delete" />
+        </div>
+    <div class="bd">
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+    </div>
+    </div>
+
+     <div class="his-hot">
+        <div class="hd">
+            <h4>热门搜索</h4>
+        </div>
+    <div class="bd">
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+        <van-tag plain type="default">标签</van-tag>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.his-hot{
+    background-color: #fff;
+    padding: 0.1rem;
+    margin-bottom: 0.9rem;
+    .hd{
+        display: flex;
+        justify-content: space-between; 
+        font-size: 0.50rem;
+        h4{
+            font-size:1rem;
+        }
+        margin-bottom:0.05rem;
+    }
+
+    .vant-tag{
+        margin-right: 0.5rem;
+        padding: 0.5rem;
+    }
+}
+</style>
