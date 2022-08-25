8f1eb74ce0dd8cdd51634f635b98f3bc5a7d2025
ASTER4869 <1977289249@qq.com>
2022-08-25 23:35:39 +0800

    颜色更改

--- src/common/_footer.vue
+++ src/common/_footer.vue
@@ -81,11 +81,11 @@
       box-shadow: 0 0 2.2vw 0 hsla(0, 6%, 50%, .13);
       -webkit-box-shadow: 0 0 2.2vw 0 hsla(0, 6%, 50%, .13);
       .is-selected {
-        color: @cl;
+        color: rgb(247, 45, 45);
         background-color: #fff;
         i {
           &::before {
-            color: @cl;
+            color: rgb(247, 45, 45);
           }
         }
       }
