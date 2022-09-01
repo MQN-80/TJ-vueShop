6104b60575127808cacfb1e8493ff55d73103258
tangpingxtc <2984286838@qq.com>
2022-09-01 10:20:53 +0800

    9-1-sxl

--- src/vuex/modules/login.js
+++ src/vuex/modules/login.js
@@ -2,16 +2,12 @@ import Util from '../../util/common'
 import * as types from '../types'
 const state = {
   token:0,
-  showAll:1,
   message:[]
 }
 
 const mutations = {
   [types.CHANGE_TOKEN] () {
     state.token = res
-  },
-  changeshowAll(state,res){
-      state.showAll = res
   },
   changemessage(states,res){
     state.message = res
