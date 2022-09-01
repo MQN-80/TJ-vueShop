481c187f3ea8ef4e26a6eb9d46d01f7db66c3bca
GKww <2352521749@qq.com>
2022-09-02 00:50:03 +0800

    分类页与查询页联动完成

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,7 +1,12 @@
 <template lang="html">
   <body>
       <div>
-        <!-- 圆边搜索框 -->
+      <van-nav-bar
+        :title="this.title"
+        left-text="返回"
+        left-arrow
+        @click-left="onClickLeft"/>
+         <!-- 圆边搜索框 -->
           <form action="/">
           <van-search
           v-model="searchval"
@@ -27,26 +32,31 @@
 </template>
 
 <script>
-import History from "@/components/search/History"
-import SearchProducts from "@/components/search/SearchProducts"
-import { Toast } from 'vant';
+import History from "@/components/search/History";
+import SearchProducts from "@/components/search/SearchProducts";
+import { Toast } from "vant";
 
 export default {
   data() {
     return {
       //搜索的文字，由用户输入
-      searchval:"",
+      searchval: "",
       //为1表示历史记录和热门搜索
       //为2表示展示搜索内容
-      blockshow:1,
+      blockshow: 1,
       //历史记录的列表数据
-      HistoryList:[],
+      HistoryList: [],
+      //是否按类别搜索，2是是，1是否（1为利用名称搜索）
+      type: 1,
+      title:''
     };
   },
 
   methods: {
+    onClickLeft(){
+        this.$router.push({name:'分类页'})
+    },
     onSearch(val) {
-
       // if (this.val !== "") {  //判断输入框的值
       //   // 每次搜索的值push到新数组里
       //   this.newArr.push(this.val);
@@ -55,50 +65,77 @@ export default {
       //   for (let i = this.newArr.length; i > 0; i--) {  //数组倒序  最新输入的排在最上面
       //     list.push(this.newArr[i - 1]);
       //   }
-        
+
       //   if (list.length > 10) {  //最多保存10条
       //     list = list.slice(0, 10);
       //   }
       //   localStorage.setItem("HistoryList", JSON.stringify(list));   //存localStorage
       // }
       // 发送搜索请求
-      this.get_serachProduct(val);
-
+      if (this.type == 1) {
+        this.get_serachProductbyName(val);
+        console.log("1111111111111111111111111111111111111")
+      } else if (this.type == 2) {
+        this.get_serachProductbyType(val);
+        console.log("2222222222222222222222222222222222222")
+     }
     },
     onCancel() {
       // 取消搜索请求
-      this.blockshow=1;
-      Toast('取消');
-      
+      this.blockshow = 1;
+      Toast("取消");
     },
     //获取搜索的商品
-    get_serachProduct(val){
-    this.$net({
-    method:'get',
-    url:'/ShopTransaction/search_productInfo',
-    params:{
-       product_name:val
-      }
-    }).then((response)=>{
-    this.$store.commit('changemessage',response.data) 
-    this.blockshow=2;
-    }).catch((err)=>{
-      console.log(err);
-    })
+    get_serachProductbyName(val) {
+      this.$net({
+        method: "get",
+        url: "/ShopTransaction/search_productInfo",
+        params: {
+          product_name: val,
+        },
+      })
+        .then((response) => {
+          this.$store.commit("changemessage", response.data);
+          this.blockshow = 2;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    get_serachProductbyType(val) {
+      this.$net({
+        method: "get",
+        url: "/ShopTransaction/search_product_type",
+        params: {
+          product_type: val,
+        },
+      })
+        .then((response) => {
+          this.$store.commit("changemessage", response.data);
+/*           console.log(val+"!!!!!!!!!!!!!!")
+ */          this.blockshow = 2;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
   },
-  components:{
+  components: {
     History,
-    SearchProducts
+    SearchProducts,
   },
-  mounted(){
-     
+  beforeMount(){
+      if(this.$route.params.type){
+        this.type=this.$route.params.type;
+        this.searchval=this.$route.params.val;
+      }
+      if(this.type==1){this.title="以商品名称搜索"}else {this.title="以商品类别搜索"}
   }
 };
 </script>
 
 <style lang="less" scoped>
-body{
+body {
   background-color: #eee;
 }
 </style>
